cd7be35df6daf83895332df42b588915a4f964c3
Justin Morales <jmoralespriv@gmail.com>
2026-07-09 21:17:55 -0400

    typo

--- handlers/health.go
+++ handlers/health.go
@@ -6,12 +6,12 @@ import (
 )
 
 type HealthResponse struct {
-	status string `json:"status"`
+	Status string `json:"status"`
 }
 
 func (h *Handler) HealthHandler(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "application/json")
 	w.WriteHeader(http.StatusOK)
 
-	json.NewEncoder(w).Encode(HealthResponse{status: "ok"})
+	json.NewEncoder(w).Encode(HealthResponse{Status: "ok"})
 }
